0603f923be24e27a984799f6f9942d0eff6aad6b
riday-kumar <hridoykumarsaha07@gmail.com>
2026-06-14 09:33:03 +0600

    query - 6 done

--- query.sql
+++ query.sql
@@ -81,3 +81,8 @@ inner join matches using (match_id);
 -- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
 select user_id, full_name, booking_id 
 from users left join bookings using(user_id);
+
+
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+select booking_id, match_id, total_cost from bookings
+where total_cost > (select avg(total_cost) from bookings);
